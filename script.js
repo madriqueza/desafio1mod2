5c350bc6e2674429ce50ef00f9539fb26f7d9e40
madriqueza <henrique2mad@gmail.com>
2022-01-19 22:04:55 -0300

    Fiz o HTML e o exercício de JS.

---
+++ script.js
@@ -0,0 +1,24 @@
+// Crie uma variável que receba um nome;
+
+// Crie uma variável que receba um número;
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+
+let name = "Henrique"
+let number = 22
+let year = 2022
+let quartaFeira = true
+
+
+console.log(number + year)
+console.log("Estou mexendo no console.log")
+console.log(quartaFeira)
